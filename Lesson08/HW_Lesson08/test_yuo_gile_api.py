d01221305f4376a1872f0bd1f194be663a57b0e3
IvanVl94 <123305766+IvanVl94@users.noreply.github.com>
2025-03-19 11:16:34 +0300

    Измененное ДЗ

--- Lesson08/HW_Lesson08/test_yuo_gile_api.py
+++ Lesson08/HW_Lesson08/test_yuo_gile_api.py
@@ -1,185 +1,74 @@
-
 import pytest
 import requests
 from api_client import Yougile
-
 
 base_url = "https://ru.yougile.com/api-v2/"
 
 
 @pytest.fixture
-
 def yougile_api():
-    base_url = "https://ru.yougile.com/api-v2/" 
-    token = "Bearer yOInDOF8DUYaY3sET7mPVdigX9oOwuLYNpYD3i2ftx4C5Mhoil-s09F9oKRCz4iY"           
-               
+    base_url = "https://ru.yougile.com"
+    token = "Bearer yOInDOF8DUYaY3sET7mPVdigX9oOwuLYNpYD3i2ftx4C5Mhoil-s09F9oKRCz4iY"
+
     return Yougile(base_url, token)
 
- 
 
 # Добавить проект (позитивная проверка)
 
 def test_create_project_positive(yougile_api):
-    project_data = {
-        "title": "Python-8.3",
-        "users": {
-            "df9bcca1-2bdf-4722-b105-f09631290479": "admin"}
-        }
+    project_data = {"title": "Python-8.3"}
     response = yougile_api.create_project(project_data)
-    assert response.status_code == 201
-    assert response.json().get("name") == project_data["name"]
-    
+    assert response.status_code == 201, f"Response Body:{response.text}"
+
+
 # Добавить проект (позитивная проверка)
 def test_create_project_negative(yougile_api):
     project_data = {}
     response = yougile_api.create_project(project_data)
-    assert response.status_code == 404
-    
-    
+    assert response.status_code == 400
+
+
 # Изменить Id проекта
 
 def test_update_project_positive(yougile_api):
-    project_id = "80916100-78bc-4ae1-a29d-73503360e9a0"
-    project_data = {
-        "deleted": True,
-        "title": "Python-8.3",
-        "users": {
-            "df9bcca1-2bdf-4722-b105-f09631290479": "admin"}
-    }
-    
-    response = yougile_api.update_project(project_data, project_id)
-    assert response.status_code == 200
-    
+    project_data = {"title": "Python-8.3"}
+    response = yougile_api.create_project(project_data)
+    project_id = response.json()["id"]
+
+    project_data_2 = {"title": "Python-8.4"}
+    response = yougile_api.update_project(project_id, project_data_2)
+    assert response.status_code == 200, f"Response Body:{response.text}"
+
+
 def test_update_project_negative(yougile_api):
     project_id = 99999
-    project_data = {
+    project_id = {
         "deleted": True,
         "title": "Python-8.3",
         "users": {
             "df9bcca1-2bdf-4722-b105-f09631290479": "admin"}
     }
-    
-    response = yougile_api.update_project(project_data, project_id)
-    assert response.status_code == 401
-    
+
+    response = yougile_api.update_project(project_id, project_id)
+    assert response.status_code == 404
+
+
 # Изменить получить по  Id
 
 def test_get_project_positive(yougile_api):
-    project_id = "80916100-78bc-4ae1-a29d-73503360e9a0"
-    
+    project_data = {"title": "Python-8.3"}
+    response = yougile_api.create_project(project_data)
+    project_id = response.json()["id"]
+
     response = yougile_api.get_project(project_id)
     assert response.status_code == 200
-    assert response.json()("id") == project_id["id"]
+    assert response.json()["id"] == project_id
+
 
 def test_get_project_negative(yougile_api):
     project_id = 99999
-    
-    response = yougile_api.get_project(project_id)
-    assert response.status_code == 401
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def test_create_project_positive(yougile_api):
-    project_data = {
-        "name": "...",
-        "description": "..."
-    }
-    response = yougile_api.create_project(project_data)
-    assert response.status_code == 201
-    assert response.json().get("name") == project_data["name"]
-
-def test_create_project_negative(yougile_api):
-    project_data = { }
-    response = yougile_api.create_project(project_data)
-    assert response.status_code == 400                     
-
-def test_update_project_positive(yougile_api):
-    project_id = 1                                          
-    project_data = {
-        "name": "",
-        "description": ""
-    }
-    response = yougile_api.update_project(project_id, project_data)
-    assert response.status_code == 200
-    assert response.json().get("name") == project_data["name"]
-
-# Не существующий ID
-def test_update_project_negative(yougile_api):
-    project_id = 9999                                              
-    project_data = {
-        "name": "New Project",
-        "description": "This project won't be created."
-    }
-    response = yougile_api.update_project(project_id, project_data)
-    assert response.status_code == 404                               
-
-# Замените на существующий ID проекта
-def test_get_project_positive(yougile_api):
-    project_id = 1  
-    response = yougile_api.get_project(project_id)
-    assert response.status_code == 200
-    assert response.json().get("id") == project_id
-
-# Не существующий ID
-def test_get_project_negative(yougile_api):
-    project_id = 9999                                    # Не существующий ID
     response = yougile_api.get_project(project_id)
     assert response.status_code == 404
+
+
